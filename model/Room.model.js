54a9ef83133958e2b6c6d953eac7415e0c3773f1
Maxwell Takyi <72990512+damagermax@users.noreply.github.com>
2023-12-17 16:47:15 +0000

    refactor

--- model/Room.model.js
+++ model/Room.model.js
@@ -1,18 +1,32 @@
 import mongoose from "mongoose";
 
-const RoomSchema = new mongoose.Schema(
+const roomSchema = new mongoose.Schema(
   {
     number: { type: String, required: true, unique: [true, "Room already exist"] },
     price: { type: Number, required: true },
     type: { type: String, required: true },
     floor: { type: String, required: true },
+    number_of_bed: { type: Number, min: 0 },
     bed_left: { type: Number, min: 0 },
     gender: { type: String, enum: ["male", "female"], required: true },
   },
   { timestamps: true }
 );
 
-RoomSchema.set("toJSON", {
+roomSchema.pre("save", function (next) {
+  // Extract the first number using a regular expression
+  const matches = this.type.match(/\d+/);
+
+  if (matches && matches.length > 0) {
+    const bedCount = parseInt(matches[0], 10);
+    this.number_of_bed = bedCount;
+    this.bed_left = bedCount;
+  }
+
+  next();
+});
+
+roomSchema.set("toJSON", {
   virtuals: true,
   versionKey: false,
   transform: function (doc, ret) {
@@ -20,4 +34,4 @@ RoomSchema.set("toJSON", {
   },
 });
 
-export default mongoose.model("room", RoomSchema);
+export default mongoose.model("room", roomSchema);
