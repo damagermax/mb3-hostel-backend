8b8da58eea4cc1083f176e22e44b59c9c0e1a407
Maxwell Takyi <72990512+damagermax@users.noreply.github.com>
2023-12-18 00:20:32 +0000

    refactor

--- controller/booking.controller.js
+++ controller/booking.controller.js
@@ -7,7 +7,16 @@ import ErrorResponse from "../utils/errorResponse.js";
 export const createBooking = asyncHandler(async (req, res) => {
   console.log("\n======================================WEBHOOK==========================");
 
-  await Booking.create({ email: "m11111w@gmail.com", room_number: "30", full_name: "hermes" });
+  const {
+    event,
+    metadata,
+    data: { paid, paid_at, amount, reference, channel },
+  } = req.body;
 
-  res.send(200);
+  if (event == "charge.success") {
+    console.log(req.body);
+    await Booking.create({ paid, paid_at, amount, reference, channel, ...metadata });
+  }
+
+  res.sendStatus(200);
 });
--- model/Booking.model.js
+++ model/Booking.model.js
@@ -3,8 +3,17 @@ import mongoose from "mongoose";
 const BookingSchema = new mongoose.Schema(
   {
     email: { type: String, trim: true, required: [true, "Please add an email"] },
-    room_number: { type: String, required: true },
     full_name: { type: String, required: true },
+    phone: { type: String, required: true },
+    gender: { type: String, required: true },
+    phone: { type: String, required: true },
+    room: { type: mongoose.Types.ObjectId, required: true },
+    school: { type: mongoose.Types.ObjectId, required: true },
+    paid: { type: String, required: true },
+    paid_at: { type: String, required: true },
+    reference: { type: String, required: true },
+    amount: { type: String, required: true },
+    channel: { type: String, required: true },
   },
   { timestamps: true }
 );
